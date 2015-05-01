7846641f460024c771f77e390b3004b98cbd014f
linkfirefox <15657960819@163.com>
2015-05-02 03:51:28 +0800

    Improve the README file

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get 'about'   =>'static_pages#about'
   get 'contact'   => 'static_pages#contact'
    get 'signup'  => 'users#new'
+   get 'blog'   =>'blog#new'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
